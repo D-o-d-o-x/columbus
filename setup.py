39c5c333483e4d8a58b31a5f80ff4d66a97946b8
Dominik Roth <dominik.roth.dev@gmail.com>
2022-06-19 16:37:53 +0200

    Converted to python-package

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
+
 setup(
-    name='Columbus',
-    packages=find_packages(),
+    name='columbus',
+    version='1.0.0',
+    # url='https://github.com/mypackage.git',
+    # author='Author Name',
+    # author_email='author@gmail.com',
+    # description='Description of my package',
+    packages=['.'],
+    # install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
 )
